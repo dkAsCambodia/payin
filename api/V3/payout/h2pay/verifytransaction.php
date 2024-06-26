0a7c0b414c9e4a3e171462f7176b6bad2f673515
dkAsCambodia <nitindamo50@gmail.com>
2024-06-26 15:21:58 +0700

    setup new payin verification URL

--- api/V3/payout/h2pay/verifytransaction.php
+++ api/V3/payout/h2pay/verifytransaction.php
@@ -1,9 +1,11 @@
 <?php
-if(!empty($_POST['transId']) && !empty($_POST['key'])){
-    return "True";
-}elseif(!empty($_GET['transId']) && !empty($_GET['key'])){
-    return "True";
-}else{
-    echo "working no have data";
-    return false;
-}
+$val="True";
+return $val;
+// if(!empty($_POST['transId']) && !empty($_POST['key'])){
+//     return "True";
+// }elseif(!empty($_GET['transId']) && !empty($_GET['key'])){
+//     return "True";
+// }else{
+//     echo "working no have data";
+//     return false;
+// }
